7d92d818597deb28928e132c6ebd7bb3efff90e5
elaintran <elain.ttran@gmail.com>
2019-07-30 16:30:30 -0500

    Updates

---
+++ precache-manifest.afb7ae9bdc05a29253cbc0e8e9157446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134110deb5746bb84403ea340b62595e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4b1a4841f4f80178c012",
+    "url": "/clicky-game/static/css/main.9a141c95.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4b1a4841f4f80178c012",
+    "url": "/clicky-game/static/js/main.b16e27ea.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
